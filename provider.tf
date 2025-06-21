cb78362ddffb550edde15e5fa0db3838a2f86962
Steven <sc0.steven.chen@gmail.com>
2025-06-20 21:35:30 -0500

    Initial Commit!!!

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+        proxmox = {
+            source  = "bpg/proxmox"
+            version = ">= 0.78.2"
+        }
+        talos = {
+            source = "siderolabs/talos"
+            version = ">= 0.8.1"
+        }
+    }
+    required_version = ">= 1.12.2"
+}
+
+provider "proxmox" {
+    endpoint  = var.endpoint
+    api_token = var.api_token
+}
+
+provider "talos" {
+
+}
