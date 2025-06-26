a57d91fa1e1f4873146a3893d6379a905537fad5
Steven <sc0.steven.chen@gmail.com>
2025-06-26 15:16:47 -0500

    removed script

--- create_cluster.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-terraform init
-terraform validate
-terraform apply -var-file terraform.tfvars
-terraform output -raw talosconfig > talosconfig
-terraform output -raw kubeconfig > .kube/config
