c2db06e0cfdb0a65e550ee81b49b95bee5988fd7
Steven <sc0.steven.chen@gmail.com>
2025-06-27 20:33:06 -0500

    moved set config for helm all into a separate yaml

--- modules/cilium/main.tf
+++ modules/cilium/main.tf
@@ -13,57 +13,5 @@ resource "helm_release" "cilium" {
   version    = "1.17.5"
   namespace  = "kube-system"
 
-  set = [
-    {
-      name  = "ipam.mode"
-      value = "kubernetes"
-    },
-
-    {
-      name  = "kubeProxyReplacement"
-      value = "true"
-    },
-
-    {
-      name  = "securityContext.capabilities.ciliumAgent"
-      value = "{CHOWN,KILL,NET_ADMIN,NET_RAW,IPC_LOCK,SYS_ADMIN,SYS_RESOURCE,DAC_OVERRIDE,FOWNER,SETGID,SETUID}"
-    },
-
-    {
-      name  = "securityContext.capabilities.cleanCiliumState"
-      value = "{NET_ADMIN,SYS_ADMIN,SYS_RESOURCE}"
-    },
-
-    {
-      name  = "cgroup.autoMount.enabled"
-      value = "false"
-    },
-
-    {
-      name  = "cgroup.hostRoot"
-      value = "/sys/fs/cgroup"
-    },
-
-    {
-      name  = "k8sServiceHost"
-      value = "localhost"
-    },
-
-    {
-      name  = "k8sServicePort"
-      value = "7445"
-    },
-    {
-      name  = "gatewayAPI.enabled"
-      value = "true"
-    },
-    {
-      name  = "gatewayAPI.enableAlpn"
-      value = "true"
-    },
-    {
-      name  = "gatewayAPI.enableAppProtocol"
-      value = "true"
-    }
-  ]
+  values = [file("${path.module}/configs/cilium.yaml")]
 }
