022ef3e316fca9bf394d0d5383a6ed5af0472aca
Steven <sc0.steven.chen@gmail.com>
2025-06-27 20:34:03 -0500

    longhorn csi

--- modules/longhorn/main.tf
+++ modules/longhorn/main.tf
@@ -1,8 +1,16 @@
-# resource "helm_release" "longhorn" {
-#   name             = "longhorn-csi"
-#   repository       = "https://charts.longhorn.io"
-#   chart            = "longhorn"
-#   version          = "1.8.2"
-#   create_namespace = true
-#   namespace        = "longhorn-system"
-# }
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+  }
+}
+
+resource "helm_release" "longhorn" {
+  name             = "longhorn-csi"
+  repository       = "https://charts.longhorn.io"
+  chart            = "longhorn"
+  version          = "1.8.2"
+  namespace        = "longhorn-system"
+  create_namespace = true
+}
