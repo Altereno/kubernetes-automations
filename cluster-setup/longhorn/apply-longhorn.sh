7b0a809734f5cf195c0dc69fc89bd107de44e31c
Steven <sc0.steven.chen@gmail.com>
2025-07-12 13:18:56 -0500

    create namespace longhorn

--- cluster-setup/longhorn/apply-longhorn.sh
+++ cluster-setup/longhorn/apply-longhorn.sh
@@ -1,11 +1,11 @@
+kubectl apply -f longhorn-namespace.yaml
+
 helm repo add longhorn https://charts.longhorn.io
 helm repo update
-kubectl apply -f longhorn-namespace.yaml
 helm upgrade --install \
     longhorn longhorn/longhorn \
     --version 1.9.0 \
     --namespace longhorn-system
 
 kubectl label namespaces longhorn-system shared-gateway-access=true
-
 kubectl apply -f httproute.yaml
