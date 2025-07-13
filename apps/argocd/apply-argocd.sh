c14506451bda38af919512906fe0f18c26aeec7d
Steven <sc0.steven.chen@gmail.com>
2025-07-12 23:02:36 -0500

    updated argocd script

--- apps/argocd/apply-argocd.sh
+++ apps/argocd/apply-argocd.sh
@@ -1,8 +1,11 @@
 kubectl create namespace argocd
 kubectl label ns argocd shared-gateway-access=true
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl wait --for=condition=available deployment --all --namespace=argocd --timeout=60s
 
 kubectl patch configmap argocd-cmd-params-cm -n argocd --type merge -p '{"data":{"server.insecure":"true"}}'
 kubectl rollout restart deployment argocd-server -n argocd
+
+kubectl apply -f http-route.yaml
 
 kubectl get secrets -n argocd argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 --decode
